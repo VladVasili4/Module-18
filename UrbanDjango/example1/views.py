2e90d41d612c65b68263f4ae6272c2c0b5ab57b0
Vlad <dipol777@ya.ru>
2024-10-31 14:56:14 +0300

    Посмотрел лекцию про шаблоны DTL в Джанго, изменил index.html в templates и views.py в example1

--- UrbanDjango/example1/views.py
+++ UrbanDjango/example1/views.py
@@ -3,7 +3,19 @@ from django.views.generic import TemplateView
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    text = 'About LOVE'
+    name = 'Viktor'
+    number = 777
+    list = ['3.785642', 'ttt', 'yyy']
+    len_list = len(list)
+    context = {
+        'text': text,
+        'name': name,
+        'number': number,
+        'list': list,
+        'len_list': len_list
+    }
+    return render(request, 'index.html', context)
 
 # class index2(TemplateView):
 #     template_name = 'index2.html' причины того, почему закомментировали, расписаны в urls. Мы не стали делать
